dc5262ed27d1eeaa03e5b47ce3311d23c6411499
Umesh <umesh.krishnamurthy92@gmail.com>
2017-10-03 11:48:12 +0530

    Commit - Dagger 2 Component Till Rebuild App Part - > No Error

--- app/src/main/java/com/example/umesh/chintushop/core/ChintuShopApplication.java
+++ app/src/main/java/com/example/umesh/chintushop/core/ChintuShopApplication.java
@@ -2,9 +2,28 @@ package com.example.umesh.chintushop.core;
 
 import android.app.Application;
 
+import com.example.umesh.chintushop.core.dagger.AppComponent;
+
 /**
  * Created by Umesh on 03-10-2017.
  */
 
 public class ChintuShopApplication extends Application{
+
+    private static AppComponent appComponent;
+    private static ChintuShopApplication instance = new ChintuShopApplication();
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        getAppComponent();
+    }
+
+    private void getAppComponent() {
+
+    }
+
+
 }
--- app/src/main/java/com/example/umesh/chintushop/core/dagger/AppComponent.java
+++ app/src/main/java/com/example/umesh/chintushop/core/dagger/AppComponent.java
@@ -1,8 +1,21 @@
 package com.example.umesh.chintushop.core.dagger;
 
+import com.example.umesh.chintushop.common.MainActivity;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
 /**
  * Created by Umesh on 21-09-2017.
  */
-
+@Singleton
+@Component(
+        modules = {
+                AppModule.class,
+                ShoppingCartModule.class
+        }
+)
 public interface AppComponent {
+    void inject(MainActivity activity);
 }
