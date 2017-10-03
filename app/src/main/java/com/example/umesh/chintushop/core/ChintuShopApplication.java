3043b1ae7070c833bf868e3f0199105f7074813e
Umesh <umesh.krishnamurthy92@gmail.com>
2017-10-03 11:54:01 +0530

    Commit- Done With Dagger 2 App Components -> No Error

--- app/src/main/java/com/example/umesh/chintushop/core/ChintuShopApplication.java
+++ app/src/main/java/com/example/umesh/chintushop/core/ChintuShopApplication.java
@@ -3,6 +3,8 @@ package com.example.umesh.chintushop.core;
 import android.app.Application;
 
 import com.example.umesh.chintushop.core.dagger.AppComponent;
+import com.example.umesh.chintushop.core.dagger.AppModule;
+import com.example.umesh.chintushop.core.dagger.DaggerAppComponent;
 
 /**
  * Created by Umesh on 03-10-2017.
@@ -22,6 +24,11 @@ public class ChintuShopApplication extends Application{
     }
 
     private void getAppComponent() {
+        if (appComponent == null){
+            appComponent = DaggerAppComponent.builder()
+                    .appModule(new AppModule(this))
+                    .build();
+        }
 
     }
 
