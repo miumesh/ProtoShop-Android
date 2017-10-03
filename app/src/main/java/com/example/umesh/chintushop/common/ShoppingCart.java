66bd43e105955c0b80d7b23691f28414dc8dad90
Umesh <umesh.krishnamurthy92@gmail.com>
2017-10-03 11:04:40 +0530

    Seventh Commit - Done Shopping Cart Class Next -> Dagger 2

--- app/src/main/java/com/example/umesh/chintushop/common/ShoppingCart.java
+++ app/src/main/java/com/example/umesh/chintushop/common/ShoppingCart.java
@@ -1,8 +1,160 @@
 package com.example.umesh.chintushop.common;
 
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.example.umesh.chintushop.model.Customer;
+import com.example.umesh.chintushop.model.LineItem;
+import com.example.umesh.chintushop.util.Constants;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Umesh on 21-09-2017.
  */
 
-public class ShoppingCart {
+public class ShoppingCart implements ShoppingCartContract{
+    private List<LineItem> shoppingCart;
+    private Customer selectedCustomer;
+    private final SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    private final static String LOG_TAG = ShoppingCart.class.getSimpleName();
+    private static boolean DEBUG = true;
+
+    public ShoppingCart(SharedPreferences sharedPreferences) {
+        this.sharedPreferences = sharedPreferences;
+        initShoppingCart();
+    }
+
+    private void initShoppingCart() {
+        shoppingCart = new ArrayList<>();
+        selectedCustomer = new Customer();
+        Gson gson = new Gson();
+
+        //check if there are items saved to the Shared Preference
+        if (sharedPreferences.getBoolean(Constants.OPEN_CART_EXISTS, false)){
+            String serializedCartItems = sharedPreferences.getString(Constants.SERIALIZED_CART_ITEMS, "");
+            if (DEBUG){
+                Log.d(LOG_TAG, "Serialized Cart Items: " +  serializedCartItems);
+            }
+            String serializedCustomer = sharedPreferences.getString(Constants.SERIALIZED_CART_CUSTOMER, "");
+
+            if (!serializedCartItems.equals("")){
+                shoppingCart = gson.<ArrayList<LineItem>>fromJson(serializedCartItems,
+                        new TypeToken<ArrayList<LineItem>>(){}.getType());
+            }
+
+            if (!serializedCustomer.equals("")){
+                selectedCustomer = gson.fromJson(serializedCustomer, Customer.class);
+            }
+        }
+        populateToolbar();
+    }
+
+    private void populateToolbar() {
+    }
+
+    public void saveCartToPreference(){
+        if (shoppingCart != null){
+            Gson gson = new Gson();
+            String serializedItems = gson.toJson(shoppingCart);
+            if (DEBUG){
+                Log.d(LOG_TAG, "Saving Serialized Cart Items: " + serializedItems);
+            }
+
+            String serializedCustomer = gson.toJson(selectedCustomer);
+            if (DEBUG){
+                Log.d(LOG_TAG, "Saving Serialized customer Items: " + serializedCustomer);
+            }
+
+            editor.putString(Constants.SERIALIZED_CART_ITEMS, serializedItems).commit();
+            editor.putString(Constants.SERIALIZED_CART_CUSTOMER, serializedCustomer).commit();
+            editor.putBoolean(Constants.OPEN_CART_EXISTS,true).commit();
+        }
+    }
+
+
+    @Override
+    public void addItemToCart(LineItem item) {
+        boolean isItemInCart = false;
+        int itemPosition = 0;
+
+        for (LineItem tempItem: shoppingCart){
+            if (tempItem.getId() == item.getId()){
+                itemPosition = shoppingCart.indexOf(tempItem);
+                isItemInCart = true;
+                LineItem selectedItem = shoppingCart.get(itemPosition);
+                selectedItem.setQuantity(tempItem.getQuantity() + item.getQuantity());
+                shoppingCart.set(itemPosition, selectedItem);
+                break;
+            }
+        }
+
+        if (!isItemInCart){
+            shoppingCart.add(item);
+        }
+
+    }
+
+    @Override
+    public void removeItemFromCart(LineItem item) {
+        shoppingCart.remove(item);
+
+    }
+
+    @Override
+    public void clearAllItemsFromCart() {
+        shoppingCart.clear();
+
+    }
+
+    @Override
+    public List<LineItem> getShoppingCart() {
+        return shoppingCart;
+    }
+
+    @Override
+    public void setCustomer(Customer customer) {
+        selectedCustomer = customer;
+
+    }
+
+    @Override
+    public void updateItemQty(LineItem item, int qty) {
+        boolean itemAlreadyInCart = false;
+        int itemPosition = 0;
+
+        for (LineItem tempItem: shoppingCart){
+            if (tempItem.getId() == item.getId()){
+                itemPosition = shoppingCart.indexOf(tempItem);
+                LineItem itemInCart = shoppingCart.get(itemPosition);
+                itemInCart.setQuantity(qty);
+                shoppingCart.set(itemPosition, itemInCart);
+                itemAlreadyInCart = true;
+                break;
+            }
+        }
+        if (!itemAlreadyInCart){
+            item.setQuantity(qty);
+            shoppingCart.add(item);
+        }
+        populateToolbar();
+
+
+    }
+
+    @Override
+    public Customer getSelectedCustomer() {
+        return selectedCustomer;
+    }
+
+    @Override
+    public void completeCheckout() {
+        shoppingCart.clear();
+
+    }
 }
--- app/src/main/java/com/example/umesh/chintushop/util/Constants.java
+++ app/src/main/java/com/example/umesh/chintushop/util/Constants.java
@@ -5,4 +5,7 @@ package com.example.umesh.chintushop.util;
  */
 
 public class Constants {
+    public static final String SERIALIZED_CART_ITEMS = "serialized_Cart_items" ;
+    public static final String SERIALIZED_CART_CUSTOMER = "serialized_customer";
+    public static final String OPEN_CART_EXISTS = "open_cart_exists" ;
 }
