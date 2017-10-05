ea212bf2fa3ca72933a872b65cab1e0709e9a967
Umesh <umesh.krishnamurthy92@gmail.com>
2017-10-05 20:24:09 +0530

    Commit-Implemented Constructor & Methods in Database Class -> Database Helper-> No Error

--- app/src/main/java/com/example/umesh/chintushop/data/DatabaseHelper.java
+++ app/src/main/java/com/example/umesh/chintushop/data/DatabaseHelper.java
@@ -1,8 +1,27 @@
 package com.example.umesh.chintushop.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Umesh on 21-09-2017.
  */
 
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
