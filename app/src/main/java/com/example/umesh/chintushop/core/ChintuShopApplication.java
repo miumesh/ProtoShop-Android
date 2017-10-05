23a563091d994e1f2b4e05f83afd3d34141e0d11
Umesh <umesh.krishnamurthy92@gmail.com>
2017-10-05 19:59:18 +0530

    Commit-Updated Application File- > Contains Error

--- app/src/main/java/com/example/umesh/chintushop/core/ChintuShopApplication.java
+++ app/src/main/java/com/example/umesh/chintushop/core/ChintuShopApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.example.umesh.chintushop.core.dagger.AppComponent;
 import com.example.umesh.chintushop.core.dagger.AppModule;
 import com.example.umesh.chintushop.core.dagger.DaggerAppComponent;
+import com.squareup.otto.Bus;
 
 /**
  * Created by Umesh on 03-10-2017.
@@ -15,6 +16,12 @@ public class ChintuShopApplication extends Application{
     private static AppComponent appComponent;
     private static ChintuShopApplication instance = new ChintuShopApplication();
 
+    private Bus bus;
+    public Bus getBus()
+    {
+        return bus;
+    }
+
     public static ChintuShopApplication getInstance() {
         return instance;
     }
@@ -23,6 +30,7 @@ public class ChintuShopApplication extends Application{
     public void onCreate() {
         super.onCreate();
         getAppComponent();
+        instance.bus = new Bus();
     }
 
     public AppComponent getAppComponent() {
